408a67f9d94730dd6ccef5d75f1c291d2c9a5c3b
vspaz <vladimir.edelman@gmail.com>
2024-07-20 16:39:35 +0200

    bump version

--- consumers/build.gradle.kts
+++ consumers/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation("org.apache.kafka:kafka-clients:3.1.2")
+    implementation("org.apache.kafka:kafka-clients:3.1.3")
     implementation("org.slf4j:slf4j-api:2.0.13")
     implementation("org.slf4j:slf4j-simple:2.0.13")
 }
